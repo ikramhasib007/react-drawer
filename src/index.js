dc458859fb0f37f1092a788cf35489de5135a690
Jyoti Bharti <76005177+stackpointer1@users.noreply.github.com>
2021-05-12 20:29:52 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
-import ImageUpload from './components/ImageUpload';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import registerServiceWorker from "./registerServiceWorker";
+ import App from "./App";
 
-ReactDOM.render(<ImageUpload />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById("root"));
 registerServiceWorker();
